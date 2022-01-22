3a4bc625b62ffc796e73c9e03971399b75ca5de0
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-22 13:50:05 -0500

    Begin UI design for TDL. No functionality yet.

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  // const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                  ),
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todo List',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('this is a task'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('this is another task'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
