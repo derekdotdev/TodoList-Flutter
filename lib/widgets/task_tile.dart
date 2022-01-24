47cf22338f8f55b87b91a166018f3d45cc64a292
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-24 15:58:50 -0500

    Extract task tile checkbox to new widget and convert to stateful. Create bool isChecked to keep track of the state of the check box.

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,10 +5,29 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('this is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
-      ),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  State<TaskCheckBox> createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: isChecked,
+      onChanged: (bool? newValue) {
+        setState(() {
+          isChecked = newValue!;
+        });
+      },
     );
   }
 }
