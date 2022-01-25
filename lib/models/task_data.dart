e02ddb92139aa4cd7cf3de00a678028d5a1f57de
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-25 09:53:18 -0500

    Make tasks list private and create getter for it but will not let me return UnmodifiableListView<Task> as it did in the tutorial...

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/foundation.dart';
+import 'dart:collection';
 import 'task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Work out'),
     Task(name: 'Eat breakfast'),
     Task(name: 'Clean studio'),
   ];
 
+  List<Task>? get tasks {
+    return _tasks.toList();
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
   }
 }
