4610ce78a0a8d6b7a6aa75acf2b63b8d5f147cf2
Derek DiLeo <derek@Dereks-Mac-mini.local>
2022-01-25 09:29:11 -0500

    Wrap ListView.builder in a Consumer<TaskData> with value=taskData and use taskData.tasks instead of Provider.of<TaskData>(context).tasks to be more succinct.

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -6,20 +6,24 @@ import 'package:todo_flutter/models/task_data.dart';
 class TasksList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-          taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-          isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-          checkboxCallback: (bool? checkboxState) {
-            // setState(() {
-            //   // isChecked = checkboxState!;
-            //   Provider.of<TaskData>(context).tasks[index].toggleDone();
-            // });
+    return Consumer<TaskData>(
+      builder: (BuildContext context, taskData, Widget? child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              taskTitle: taskData.tasks[index].name,
+              isChecked: taskData.tasks[index].isDone,
+              checkboxCallback: (bool? checkboxState) {
+                // setState(() {
+                //   // isChecked = checkboxState!;
+                //   Provider.of<TaskData>(context).tasks[index].toggleDone();
+                // });
+              },
+            );
           },
+          itemCount: taskData.tasks.length,
         );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }
